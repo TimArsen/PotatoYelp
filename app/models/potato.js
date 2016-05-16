dcf36d655a10466a8b988b3ef588da9eb6b2568f
Tim Arsenault <timarsen@gmail.com>
2016-05-16 17:49:18 +0000

    update potato model

--- app/models/potato.js
+++ app/models/potato.js
@@ -5,6 +5,11 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('Potato', {
     name : String,
     image : String,
+    description: String,
+    country_of_origin: String,
+    best_use: String,
+    wikipedia_link: String,
+    date: { type: Date, default: Date.now },
     author: {
       id: {
          // type tells mongoose that this property is an ObjectId 
@@ -21,5 +26,7 @@ module.exports = mongoose.model('Potato', {
          // it is an ObjectId of the model exported as "User"
          ref: "Review"
     }
-    ]
+    ],
+    average_rating: { type: Number, min: 1, max: 5 },
+    num_of_reviews: Number
 });
