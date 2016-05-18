e9d1a5769f2a351403d6cf56bbebf6c2545b3f3d
Tim Arsenault <timarsen@gmail.com>
2016-05-18 19:25:40 +0000

    css changes

--- public/js/controllers/auth/LoginCtrl.js
+++ public/js/controllers/auth/LoginCtrl.js
@@ -1,7 +1,18 @@
 angular.module("potatoApp").controller("LoginController", function($scope, $http, $location, authService) {
     $scope.user = {}; // set blank user object linked to login form
     
-    $scope.userLogin = function(){ // function attached to login button
+    $scope.loginModal = false; // Hide login modal
+    // When login is required show login modal
+    $scope.$on('auth-loginRequired', function(event, data){
+        $scope.loginModal = true;
+    });
+    // When login is confirmed hide login modal
+    $scope.$on('event:auth-loginConfirmed', function(event, data){
+        $scope.loginModal = false;
+    });
+    
+    // function attached to login button
+    $scope.userLogin = function(){ 
         $http.post("/login", $scope.user) // Send login request with user data
             .then(function(res){    // wait for response
                 if(res.data.username){ // If login succesful
