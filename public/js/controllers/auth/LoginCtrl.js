aaeddb492310fd2c0852f374786e23c560c12eb9
Tim Arsenault <timarsen@gmail.com>
2016-05-18 15:54:50 +0000

    refactor login controller

--- public/js/controllers/auth/LoginCtrl.js
+++ public/js/controllers/auth/LoginCtrl.js
@@ -1,17 +1,16 @@
 angular.module("potatoApp").controller("LoginController", function($scope, $http, $location, authService) {
-    $scope.user = {};
-    $scope.userLogin = function(){
-        console.log("login running: " + $scope.user.username);
-        $http.post("/login", $scope.user)
-            .then(function(res){
-                if(res.data.username){
-                    console.log(res.data);
-                    authService.loginConfirmed(res.data);
-                    $location.path("/profile");
-                } else {
-                    $scope.error = res.data.message;
-                    $scope.user = {};
+    $scope.user = {}; // set blank user object linked to login form
+    
+    $scope.userLogin = function(){ // function attached to login button
+        $http.post("/login", $scope.user) // Send login request with user data
+            .then(function(res){    // wait for response
+                if(res.data.username){ // If login succesful
+                    authService.loginConfirmed(res.data); // prompt login success event
+                    $location.path("/profile"); // redirect to User porfile page
+                } else {  // if login unsuccessful
+                    $scope.error = res.data.message; // set error for future logging
+                    $scope.user = {};   // reset user object to blank
                 }
             });
-    }
+    };
 });
