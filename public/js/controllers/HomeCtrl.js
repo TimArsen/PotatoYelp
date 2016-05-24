4d1ab4eaaab6b4b93b017005dbf1c5931189a012
Tim Arsenault <timarsen@gmail.com>
2016-05-24 22:16:07 +0000

    link sign-up button on splash page to register modal

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,5 +1,9 @@
-angular.module('potatoApp').controller('HomeController', function($scope) {
+angular.module('potatoApp').controller('HomeController', function($scope, $rootScope) {
 
-   
+   // Show Register Function
+            $scope.showRegister = function (){
+                // Broadcast register required event when register button is clicked
+                $rootScope.$broadcast('event:auth-registerRequired', {}); 
+            };
 
 });
