128cd8a91c4e58878eb6e033a17520e266cb7014
Tim Arsenault <timarsen@gmail.com>
2016-05-24 22:29:56 +0000

    move splash behavior into home controller

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -5,5 +5,16 @@ angular.module('potatoApp').controller('HomeController', function($scope, $rootS
                 // Broadcast register required event when register button is clicked
                 $rootScope.$broadcast('event:auth-registerRequired', {}); 
             };
+            
+    // header and footer on home page
+    $(".navbar").hide();
+    $("footer").hide();
+    
+    // When clicked on login or register, unhide
+    $("a").on("click", function() {
+      $(".navbar").show();
+      $("footer").show();
+    });
+
 
 });
